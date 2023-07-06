ccad334a9478cdee42f583322af88aa4ecb1742e
Duckbababoi <135317108+Duckbababoi@users.noreply.github.com>
2023-07-06 12:58:56 +0000

    added new parts of story to innocent

--- innocent.py
+++ innocent.py
@@ -1,5 +1,30 @@
 import variables
 import random
+import sheriff
+import murderer
+import game
 
 def InnocentActions():  #действияя игрока в роли невинного
-  pass
+  boost=3
+  desision=input('You went to the bathroom and then you hear a scream, would you like to check what happened (Type Check), hide in the bathroom (Type Hide), or run in another room(type Run)?')
+  if desision.capitalize()=='Check':
+    killed=random.randint(1,6)
+    victim=random.randint(1,4)
+    
+    if killed==1:
+      print('Looks like someone from the guests decided to kill everyone')
+      if victim==1:
+        gun=input('You walk around and you find a dead sheriff would you like to pick up a gun? Yes or no?')
+        if gun.capitalize()=='Yes':
+          pass
+        if gun.capitalize()=='No':
+          pass
+      else:
+        pass
+    else: 
+      print('Murderer caught you and killed you, you lost')
+      
+      
+      
+    
+  
--- main.py
+++ main.py
@@ -24,16 +24,16 @@ def Game():
   while True:
 
     if variables.role == 1:
-      print("Your role is Innocent")
+      print("Your role is Innocent, you have 3 speed boosts to run from the murderer")
       innocent.InnocentActions()
       murderer.BotMurdererActions()
       sheriff.BotSheriff()
     elif variables.role == 2:
-      print("Your role is Sheriff")
+      print("Your role is Sheriff, you have to shoot the murderer")
       murderer.BotMurdererActions()
       sheriff.SheriffActions()
     elif variables.role == 3:
-      print("Your role is Murderer")
+      print("Your role is Murderer, kill everyone in the house")
       murderer.MurdererActions()
       sheriff.BotSheriff()
     if game.WhoWon():
